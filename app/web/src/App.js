3c5b35b2b072edaa3d90a4b94e6a0b80f0805d98
Jonas Di Dier <jonas.didier@hotmail.com>
2021-05-22 19:14:06 +0200

    Extract custom hook

--- app/web/src/App.js
+++ app/web/src/App.js
@@ -1,42 +1,10 @@
 import firebase from 'firebase/app';
-import { useEffect, useState } from 'react';
+import useRegistrationToken from './hooks/useRegistrationToken';
 
 const App = () => {
-  const [token, setToken] = useState();
+  const { token, error } = useRegistrationToken();
+
   const firebaseApp = firebase.apps[0];
-  console.log(token);
-
-  useEffect(() => {
-    const getToken = async () => {
-      // Configure Web Credentials with FCM
-      const messaging = firebase.messaging();
-
-      try {
-        const currentToken = await messaging.getToken({
-          vapidKey:
-            'BGa0XRcagyN59XPT7cFe2FWP9bxdRTfMksKADx0SAHbGvq_KhhcS9cXZv_5p9q2P_-yn8-gIJqDWR-YVF-QFWts',
-        });
-
-        if (currentToken) {
-          // Send the token to your server and update the UI if necessary
-          // ...
-          setToken(currentToken);
-          console.log(currentToken);
-        } else {
-          console.log(
-            // Show permission request UI
-            'No registration token available. Request permission to generate one.'
-            // ...
-          );
-        }
-      } catch (error) {
-        console.log('An error occurred while retrieving token. ', error);
-        // ...
-      }
-    };
-
-    getToken();
-  }, []);
 
   return (
     <>
@@ -46,7 +14,8 @@ const App = () => {
         <pre>{JSON.stringify(firebaseApp.options, null, 2)}</pre>
       </code>
 
-      <p>{token}</p>
+      {token && <p>{token}</p>}
+      {error && <p>{error}</p>}
     </>
   );
 };
--- app/web/src/index.js
+++ app/web/src/index.js
@@ -1,19 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import firebase from 'firebase/app';
-import 'firebase/messaging';
+import './services/firebase';
 import './index.css';
 import App from './App';
-
-// Initialize Firebase
-firebase.initializeApp({
-  apiKey: 'AIzaSyABANFCpUeQ9dI36d_5KVsSq_rxPVnc-Fc',
-  authDomain: 'iot-werkstuk.firebaseapp.com',
-  projectId: 'iot-werkstuk',
-  storageBucket: 'iot-werkstuk.appspot.com',
-  messagingSenderId: '420748414808',
-  appId: '1:420748414808:web:4cf1a8447ee5f5fba31f2d',
-});
 
 ReactDOM.render(
   <React.StrictMode>
