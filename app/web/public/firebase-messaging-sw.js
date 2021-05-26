07c09cc9c317489615e0df08a5ff867f25c98231
Jonas Di Dier <jonas.didier@hotmail.com>
2021-05-26 18:44:27 +0200

    🐛 Fix double notifications

--- app/web/public/firebase-messaging-sw.js
+++ app/web/public/firebase-messaging-sw.js
@@ -7,23 +7,10 @@ importScripts('/__/firebase/init.js');
 
 const messaging = firebase.messaging();
 
-// If you would like to customize notifications that are received in the
-// background (Web app is closed or not in browser focus) then you should
-// implement this optional method.
-// Keep in mind that FCM will still show notification messages automatically
-// and you should use data messages for custom notifications.
-// For more info see:
-// https://firebase.google.com/docs/cloud-messaging/concept-options
-// messaging.onBackgroundMessage(function (payload) {
-//   console.log(
-//     '[firebase-messaging-sw.js] Received background message ',
-//     payload
-//   );
-//   // Customize notification here
-//   const notificationTitle = 'Background Message Title';
+// messaging.onMessage((payload) => {
+//   const notificationTitle = 'From sw onMessage';
 //   const notificationOptions = {
-//     body: 'Background Message body.',
-//     icon: '/firebase-logo.png',
+//     body: payload,
 //   };
 
 //   self.registration.showNotification(notificationTitle, notificationOptions);
