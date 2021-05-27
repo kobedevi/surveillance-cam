502331f9f0bc331da69b098eb0136d23dc9fc429
{DanteWeverbergh} <{dantweve@student.arteveldehs.be}>
2021-05-27 16:19:42 +0200

    logout knopje

--- app/web/src/Components/App/App.js
+++ app/web/src/Components/App/App.js
@@ -1,13 +1,20 @@
 import firebase from 'firebase/app';
+import { FaSignOutAlt } from 'react-icons/fa';
 import useRegistrationToken from '../../hooks/useRegistrationToken';
 import Navbar from '../Design/Navbar/Navbar';
 
 const App = () => {
   const { token, error } = useRegistrationToken();
 
+  const logOut = () => {
+    firebase.auth().signOut();
+  };
+
   return (
     <>
       <Navbar />
+
+      <button onClick={logOut}>Uitloggen</button>
 
       <code>
         <pre>{JSON.stringify(firebase.apps[0].options, null, 2)}</pre>
