71e0144fff9e317bf330473cdf919f0da4c295a0
Jonas Di Dier <jonas.didier@hotmail.com>
2021-05-29 15:40:12 +0200

    💄 Update login page styling

--- app/web/src/index.js
+++ app/web/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
-import './services/firebase';
+import './core/services/firebase';
 import AuthContainer from './Components/App/Auth/AuthContainer';
 import './scss/main.scss';
 
