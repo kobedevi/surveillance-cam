bacad2bd737e473d5f8a664a2fff9d702120a42f
Jonas Di Dier <jonas.didier@hotmail.com>
2021-05-30 18:34:40 +0200

    💄 Timeline styling

--- app/web/src/Components/App/Timeline/Timeline.js
+++ app/web/src/Components/App/Timeline/Timeline.js
@@ -1,9 +1,29 @@
+import dayjs from 'dayjs';
+import { Fragment } from 'react';
 import { Link } from 'react-router-dom';
 import useMotionMoments from '../../../core/hooks/queries/useMotionMoments';
 import { route, Routes } from '../../../core/routing';
 import formatDate from '../../../core/utils/formatDate';
 import { Alert, Spinner, Title } from '../../Design';
 import Photo from './Photo';
+
+// Returns [{ day: '29 May', values: [...] }, { day: '28 May', values: [...] }]
+const groupByDay = (arr) => {
+  return arr.reduce((acc, obj) => {
+    const dateObj = dayjs.unix(obj.firstMotion.seconds);
+    const day = dateObj.startOf('day').format('D MMM');
+
+    const lastGroup = acc[acc.length - 1];
+
+    if (lastGroup?.day === day) {
+      lastGroup.values.push(obj);
+    } else {
+      acc.push({ day, values: [obj] });
+    }
+
+    return acc;
+  }, []);
+};
 
 const Timeline = () => {
   const { data: motionMoments, ...query } = useMotionMoments();
@@ -20,16 +40,27 @@ const Timeline = () => {
     <>
       <Title>Timeline</Title>
 
-      <ul>
-        {motionMoments.map((mm) => (
-          <li key={mm.id}>
-            <Photo path={mm.preview} />
-            <Link to={route(Routes.TimelineDetail, { id: mm.id })}>
-              {formatDate(mm.id, 'D MMM - H:mm')}
-            </Link>
-          </li>
+      <section className="timeline">
+        {groupByDay(motionMoments).map((group) => (
+          <Fragment key={group.day}>
+            <h2>{group.day}</h2>
+            <div className="timeline-group">
+              {group.values.map((mm) => (
+                <article key={mm.id}>
+                  <Photo path={mm.preview} />
+                  <p>
+                    {formatDate(mm.firstMotion, 'H:mm')}&nbsp;-&nbsp;
+                    {formatDate(mm.lastMotion, 'H:mm')}
+                  </p>
+                  <Link to={route(Routes.TimelineDetail, { id: mm.id })}>
+                    View&nbsp;recordings
+                  </Link>
+                </article>
+              ))}
+            </div>
+          </Fragment>
         ))}
-      </ul>
+      </section>
     </>
   );
 };
