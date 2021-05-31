cb577f41d36f79e158cc48155438a987a982546b
pgmgent-kobedevi <kobedevi@student.arteveldehs.be>
2021-05-31 16:26:09 +0200

    🔥 Removed tester function

--- app/pi/src/main.py
+++ app/pi/src/main.py
@@ -4,15 +4,10 @@ import Camera
 from time import sleep
 import sys
 
-def tester(running):
-    print(running)
-
 def main():
     Firebase.init()
     Camera.init()
 
-    # Attach snapshot listener and callbacks
-    # Firestore.onSettingsChange('running', changeCameraState)
     Firestore.listenToSettings()
     Camera.start()
 
